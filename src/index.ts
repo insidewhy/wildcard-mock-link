insidewhy/wildcard-mock-link

commit 78b9f2bbbc0a95fffe6a64cd0ce70f0f6160b7e6

Normalize mocked response queries by removing @client directives (#111)

src/index.ts
@@ -6,7 +6,10 @@ import {
   ApolloLink,
 } from '@apollo/client'
 import { MockedResponse } from '@apollo/client/testing'
-import { addTypenameToDocument } from '@apollo/client/utilities'
+import {
+  addTypenameToDocument,
+  removeClientSetsFromDocument,
+} from '@apollo/client/utilities'
 import delay from 'delay'
 import stringify from 'fast-json-stable-stringify'
 import {
@@ -185,8 +188,26 @@ export class WildcardMockLink extends ApolloLink {
     }
 
     mockedResponses.forEach((mockedResponse) => {
-      this.addMockedResponse(mockedResponse)
+      const normalizedMockResponse =
+        this.normalizeMockedResponse(mockedResponse)
+      this.addMockedResponse(normalizedMockResponse)
     })
+  }
+
+  /**
+   * Normalizes a WildcardMockedResponse removing @client directives from query
+   */
+  private normalizeMockedResponse(
+    mockedResponse: WildcardMockedResponse,
+  ): WildcardMockedResponse {
+    const newMockedResponse = { ...mockedResponse }
+    const newQuery = removeClientSetsFromDocument(
+      newMockedResponse.request.query,
+    )
+    if (newQuery) {
+      newMockedResponse.request.query = newQuery
+    }
+    return newMockedResponse
   }
 
   request(op: Operation): Observable<FetchResult> | null {

src/index.spec.tsx
@@ -20,6 +20,16 @@ const CAT_QUALITIES_QUERY = gql`
   }
 `
 
+const CAT_QUALITIES_QUERY_WITH_LOCAL_ONLY_FIELDS = gql`
+  query ($catName: String!) {
+    qualities(cats: $catName) {
+      loveliness
+      playfulness @client
+      friendliness @client(always: true)
+    }
+  }
+`
+
 describe('WildcardMockLink', () => {
   describe('can be used for non-subscription queries', () => {
     const useQueryOnce = (catName: string) => {
@@ -165,6 +175,40 @@ describe('WildcardMockLink', () => {
         await link.waitForLastResponse()
         expect(link.lastQueryMatches(CAT_QUALITIES_QUERY)).toBeTruthy()
         expect(link.lastQuery?.variables).toEqual({ catName: 'scruffy' })
+        expect(result.current).toEqual(data)
+      })
+    })
+
+    describe('normalizes queries with local only fields', () => {
+      it('for a single request', async () => {
+        const variables = { catName: 'Lord Fromtar' }
+        const data = {
+          qualities: {
+            __typename: 'Qualities',
+            loveliness: 'very',
+          },
+        }
+        const { wrapper, link } = hookWrapperWithApolloMocks(
+          [
+            {
+              request: {
+                query: CAT_QUALITIES_QUERY_WITH_LOCAL_ONLY_FIELDS,
+                variables,
+              },
+              result: { data },
+            },
+          ],
+          { act: actHook },
+        )
+        const { result } = renderHook(() => useQueryOnce(variables.catName), {
+          wrapper,
+        })
+        await link.waitForLastResponse()
+        expect(link.lastQueryMatches(CAT_QUALITIES_QUERY)).toBeTruthy()
+        expect(
+          link.lastQueryMatches(CAT_QUALITIES_QUERY_WITH_LOCAL_ONLY_FIELDS),
+        ).toBeFalsy()
+        expect(link.lastQuery?.variables).toEqual(variables)
         expect(result.current).toEqual(data)
       })
     })

commit 2e57d1a9069c97b9a275f722c79244c78e55c30e

Bugfix/make normalize method function (#113)
Fix lint broken by 78b9f2b

src/index.ts
@@ -138,6 +138,20 @@ const observableWithError = (error: Error): Observable<FetchResult> =>
   })
 
 /**
+ * Normalizes a WildcardMockedResponse removing @client directives from query
+ */
+const normalizeMockedResponse = (
+  mockedResponse: WildcardMockedResponse,
+): WildcardMockedResponse => {
+  const newMockedResponse = { ...mockedResponse }
+  const newQuery = removeClientSetsFromDocument(newMockedResponse.request.query)
+  if (newQuery) {
+    newMockedResponse.request.query = newQuery
+  }
+  return newMockedResponse
+}
+
+/**
  * Extends MockLink to provide the ability to match request queries independent
  * of their variables and have them match 1 or more responses. Also stores the
  * last request for use in assertion frameworks.
@@ -188,26 +202,9 @@ export class WildcardMockLink extends ApolloLink {
     }
 
     mockedResponses.forEach((mockedResponse) => {
-      const normalizedMockResponse =
-        this.normalizeMockedResponse(mockedResponse)
+      const normalizedMockResponse = normalizeMockedResponse(mockedResponse)
       this.addMockedResponse(normalizedMockResponse)
     })
-  }
-
-  /**
-   * Normalizes a WildcardMockedResponse removing @client directives from query
-   */
-  private normalizeMockedResponse(
-    mockedResponse: WildcardMockedResponse,
-  ): WildcardMockedResponse {
-    const newMockedResponse = { ...mockedResponse }
-    const newQuery = removeClientSetsFromDocument(
-      newMockedResponse.request.query,
-    )
-    if (newQuery) {
-      newMockedResponse.request.query = newQuery
-    }
-    return newMockedResponse
   }
 
   request(op: Operation): Observable<FetchResult> | null {